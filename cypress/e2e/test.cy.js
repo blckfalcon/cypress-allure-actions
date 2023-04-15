d957e2956fe04fcf48bb25f413fe4373d2c259e2
blackfalcon <blackfalcon.ru@gmail.com>
2023-04-15 02:47:29 -0300

    change minimal tests

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,10 +1,33 @@
-describe("template spec", () => {
+describe("full regression spec", () => {
     before(() => {
         cy.allure().epic("Dashboard").feature("Idea Splits").story("Create");
         cy.allure().suite("Full Regression");
     });
 
     it("passes", () => {
-        cy.visit("https://example.cypress.io");
+        expect(true).to.equal(true);
+    });
+});
+
+describe("smoke spec", () => {
+    before(() => {
+        cy.allure().epic("Dashboard").feature("Idea Splits").story("Create");
+        cy.allure().suite("Smoke");
+    });
+
+    it("passes", () => {
+        expect(true).to.equal(true);
+    });
+});
+
+
+describe("post-release spec", () => {
+    before(() => {
+        cy.allure().epic("Dashboard").feature("Idea Splits").story("Create");
+        cy.allure().suite("Post Release");
+    });
+
+    it("passes", () => {
+        expect(true).to.equal(true);
     });
 });
